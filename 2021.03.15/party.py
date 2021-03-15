8721a04f518ae4873c514e9f0df2f7694d10e2c4
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-03-15 13:07:51 +0900

    2주차 수업

---
+++ 2021.03.15/party.py
@@ -0,0 +1,27 @@
+"""
+2021.03.15 ICT2 강의
+"""
+people = input("참석자의 수를 입력하시오: ")
+#print(people)
+print("치킨의 수:",int(people),sep="")
+print("맥주의 수:",int(people)*2)
+print("케익의 수:",int(people)*4)
+#print(type(people))
+
+"""
+str => int
+way.1
+people = input("참석자의 수를 입력하시오: ")
+people = int(people)
+
+way.2
+people = int( input( "참석자의 수를 입력하시오: ") )
+"""
+"""
+sep 출력값의 공백간격과 콤마등등을 조정
+"""
+"""
+people = intput
+"""
+
+print(10, 20, sep=",") ,print("입니다.")
---
+++ 2021.03.15/word.py
@@ -0,0 +1,10 @@
+# word = 'python'
+# word[15]
+
+word1 = input("첫 번째 단어를 입력해주세요: ")
+word2 = input("두 번째 단어를 입력해주세요: ")
+word3 = input("세 번째 단어를 입력해주세요: ")
+
+# print(word1[0],word2[0],word3[0], sep="")
+
+print(word1[0]+word2[0]+word3[0])
