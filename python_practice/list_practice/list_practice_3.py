54e9b7203fc564963f8b3740bd59d1ff9f6b3eb8
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-04-25 22:23:34 +0900

    Delete list_practice_3.py

--- python_practice/list_practice/list_practice_3.py
+++
@@ -1,9 +0,0 @@
-#list practice 문제3
-number = []
-
-while True :
-    number.append(int(input("문자를 입력하시오: ")))
-    if 0 in number:
-        number.pop() # 마지막 값을 지우기 위해 사용
-        break
-print(number)
