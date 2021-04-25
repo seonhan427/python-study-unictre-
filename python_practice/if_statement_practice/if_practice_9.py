f0a39fadfd7dcb6273cb08e973ea6d75666fdc9a
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-04-25 20:30:47 +0900

    Add files via upload

---
+++ python_practice/if_statement_practice/if_practice_9.py
@@ -0,0 +1,38 @@
+# if_statement_practice 문제 9
+
+month = int(input("입력하시오: "))
+
+if month == 1:
+    print("31")
+elif month == 2:
+    print("28")
+elif month == 3:
+    print("31")
+elif month == 4:
+    print("30")
+elif month == 5:
+    print("31")
+elif month == 6:
+    print("30")
+elif month == 7:
+    print("31")
+elif month == 8:
+    print("31")
+elif month == 9:
+    print("30")
+elif month == 10:
+    print("31")
+elif month == 11:
+    print("30")
+elif month == 12:
+    print("31")
+else:
+    print("error")
+
+
+
+
+
+
+
+
