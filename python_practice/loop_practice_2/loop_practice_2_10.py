4e1ee9da74ee7009d233dad4f7be0ba3d7ca570d
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-04-25 23:20:13 +0900

    Add files via upload

---
+++ python_practice/loop_practice_2/loop_practice_2_10.py
@@ -0,0 +1,10 @@
+# practice loop 2 문제10
+
+space = 7//2
+for x in range(1,8,2):
+    print(" "*space,"*"*x," "*space)
+    space -= 1
+space2 = 1
+for x in range(5,0,-2):
+    print(" "*space2,"*"*x," "*space2)
+    space2 += 1
