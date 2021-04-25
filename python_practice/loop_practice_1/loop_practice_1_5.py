c64c49b4073b6d05d4e336e2798594793505997d
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-04-25 21:48:12 +0900

    Add files via upload

---
+++ python_practice/loop_practice_1/loop_practice_1_5.py
@@ -0,0 +1,14 @@
+# practice loop 1 문제5
+
+evencount=0
+oddcount=0
+
+while True:
+    number = int(input("입력하시오 : "))
+    if number == 0:
+        break
+    elif number % 2 != 0:
+        oddcount +=1
+    elif number % 2 == 0:
+        evencount += 1
+print("odd :",oddcount,"even :",evencount)
