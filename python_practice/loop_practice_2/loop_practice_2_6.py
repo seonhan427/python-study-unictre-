04b1f6f69e42a9610ed51b929a21b90af637c984
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-04-25 21:58:11 +0900

    Add files via upload

---
+++ python_practice/loop_practice_2/loop_practice_2_6.py
@@ -0,0 +1,11 @@
+# practice loop 2 문제6
+string = list(input("입력 문자열:"))
+# print(string)
+
+letter = input("찾고 싶은 문자:")
+string.index(letter)
+
+#enumerate 을 사용해서 여러개의 인덱스 값을 찾았습니다
+#설명이 더 필요할꺼같습니다!!
+output = [idx for idx, let in enumerate(string) if let == letter]
+print(output)
