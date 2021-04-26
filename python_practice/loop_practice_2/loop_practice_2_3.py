4cc0b92b0e836cc29cb3416cfe8951a945d1b612
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-04-26 23:19:42 +0900

    Add files via upload

---
+++ python_practice/loop_practice_2/loop_practice_2_3.py
@@ -0,0 +1,12 @@
+# practice loop 2 문제3
+
+number = int(input("입력하시오 :"))
+z = 0
+for x in range(number):
+    for y in range(x):
+        print(' ', end=' ')
+    for y in range(number-x):
+        z +=1
+        print(z,end=' ')
+    print()
+
